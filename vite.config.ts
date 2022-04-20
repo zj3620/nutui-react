51de34d045b650ca1738259c65664828503f613d
oasis-cloud <suanbanren@foxmail.com>
2022-04-20 16:15:58 +0800

    chore: 可视化主题定制

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-
+const config = require('./package.json')
 const atImport = require('postcss-import')
 const path = require('path')
 
@@ -35,9 +35,35 @@ export default defineConfig({
         additionalData: `@import "@/styles/variables.scss";@import "@/sites/assets/styles/variables.scss";`,
       },
       postcss: {
-        plugins: [atImport({ path: path.join(__dirname, 'src`') })],
+        plugins: [
+          atImport({ path: path.join(__dirname, 'src`') }),
+          require('autoprefixer')({
+            overrideBrowserslist: [
+              '> 0.5%',
+              'last 2 versions',
+              'ie > 11',
+              'iOS >= 10',
+              'Android >= 5',
+            ],
+          }),
+        ],
       },
     },
   },
   plugins: [reactRefresh()],
+  build: {
+    target: 'es2015',
+    outDir: './dist/1x/',
+    cssCodeSplit: true,
+    rollupOptions: {
+      input: {
+        mobile: resolve(__dirname, 'demo.html'),
+      },
+      output: {
+        entryFileNames: `demo-${config.version}/[name].js`,
+        chunkFileNames: `demo-${config.version}/[name].js`,
+        assetFileNames: `demo-${config.version}/[name].[ext]`,
+      },
+    },
+  },
 })
